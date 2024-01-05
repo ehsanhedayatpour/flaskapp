646542390747bc023dd3e73552fab2c487e5a104
e.hedayatpour <e.hedayatpour@alibaba.ir>
2024-01-05 21:12:51 +0330

    add /health for probe

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ def index():
     url = random.choice(images)
     return render_template("index.html", url=url)
 
+@app.route("/health")
+def health():
+    return "OK"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
